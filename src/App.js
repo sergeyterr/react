b96674c66de54f59c87cc1237567b429bdd9e087
sergeyterr <ater123@mail.ru>
2020-10-14 19:32:35 +0300

    day 1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from "./components/Header/Header"
+import Main from "./components/Main/Main"
+import Features from "./components/Features/Features"
+import Footer from "./components/Footer/Footer"
+import Calendar from "./components/Calendar/Calendar"
+import Details from "./components/Details/Details"
+import './style.css'
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Main />
+			<Features />
+			{/*<Calendar />*/}
+			{/*<Details />*/}
+			<Footer />
+		</>
+	);
+}
+
+export default App;
